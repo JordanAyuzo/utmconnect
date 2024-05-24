adf22452df7269b1d3779b6d54486598ad136cc9
Jorge Barahona <bacj010509@gs.utm.mx>
2024-05-24 12:19:57 -0600

    Se agregó la paginación a la tabla de alumnos.

--- src/components/layouts/tables/tableAlumnos.jsx
+++ src/components/layouts/tables/tableAlumnos.jsx
@@ -1,12 +1,13 @@
 import { Button } from "@/components/ui/button";
-import { Table, TableBody, TableCaption, TableCell, TableFooter, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-import { CardDescription } from "@/components/ui/card";
+import { Table, TableBody, TableCell, TableFooter, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { Pagination, PaginationContent, PaginationEllipsis, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from "@/components/ui/pagination";
 import { getAlumnos } from "@/services/alumnos/alumnoService";
 import React, { useEffect, useState } from "react";
 
 function TableAlumnos() {
-
   const [alumnos, setAlumnos] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const alumnosPerPage = 4;
 
   useEffect(() => {
     getAlumnos().then((res) => {
@@ -14,39 +15,87 @@ function TableAlumnos() {
     });
   }, []);
 
+  const indexOfLastAlumno = currentPage * alumnosPerPage;
+  const indexOfFirstAlumno = indexOfLastAlumno - alumnosPerPage;
+  const currentAlumnos = alumnos.slice(indexOfFirstAlumno, indexOfLastAlumno);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   return (
-    <Table>
-      {/* <TableCaption>Una lista de los alumnos.</TableCaption> */}
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[100px]">Nombre</TableHead>
-          <TableHead>Carrera</TableHead>
-          <TableHead>Grupo</TableHead>
-          <TableHead>Matricula</TableHead>
-          <TableHead className="text-center">Acción</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {alumnos.map((nombre) => (
-          <TableRow key={nombre.nombre}>
-            <TableCell className="font-medium">{nombre.nombre}</TableCell>
-            <TableCell>{nombre.carrera}</TableCell>
-            <TableCell>{nombre.grupo}</TableCell>
-            <TableCell>{nombre.matricula}</TableCell>
-            <TableCell className="text-center">
-              <Button variant="destructive">Dar de baja</Button>
+    <div>
+      <h2 className="scroll-m-20 border-b pb-2 text-3xl text-center font-semibold tracking-tight first:mt-0">
+        Lista de alumnos
+      </h2>
+      <Table>
+        {/* <TableCaption>Una lista de los alumnos.</TableCaption> */}
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Nombre</TableHead>
+            <TableHead>Carrera</TableHead>
+            <TableHead>Grupo</TableHead>
+            <TableHead>Matricula</TableHead>
+            <TableHead className="text-center">Acción</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {currentAlumnos.map((nombre) => (
+            <TableRow key={nombre.nombre}>
+              <TableCell className="font-medium">{nombre.nombre}</TableCell>
+              <TableCell>{nombre.carrera}</TableCell>
+              <TableCell>{nombre.grupo}</TableCell>
+              <TableCell>{nombre.matricula}</TableCell>
+              <TableCell className="text-center">
+                <Button variant="destructive">Dar de baja</Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TableCell colSpan="5" className="text-right">
+              <Button variant="primary">Agregar alumno</Button>
             </TableCell>
           </TableRow>
-        ))}
-      </TableBody>
-      <TableFooter>
-        <TableRow>
-          <TableCell colSpan="5" className="text-right">
-            <Button variant="primary">Agregar alumno</Button>
-          </TableCell>
-        </TableRow>
-      </TableFooter>
-    </Table>
+        </TableFooter>
+      </Table>
+      <Pagination>
+        <PaginationContent>
+          <PaginationPrevious>
+            <PaginationLink
+              href="#"
+              onClick={() => {
+                if (currentPage > 1) {
+                  paginate(currentPage - 1);
+                }
+              }}
+            >
+              Anterior
+            </PaginationLink>
+          </PaginationPrevious>
+          {[...Array(Math.ceil(alumnos.length / alumnosPerPage)).keys()].map(
+            (num) => (
+              <PaginationItem key={num}>
+                <PaginationLink href="#" onClick={() => paginate(num + 1)}>
+                  {num + 1}
+                </PaginationLink>
+              </PaginationItem>
+            )
+          )}
+          <PaginationNext>
+            <PaginationLink
+              href="#"
+              onClick={() => {
+                if (currentPage < Math.ceil(alumnos.length / alumnosPerPage)) {
+                  paginate(currentPage + 1);
+                }
+              }}
+            >
+              Siguiente
+            </PaginationLink>
+          </PaginationNext>
+        </PaginationContent>
+      </Pagination>
+    </div>
   );
 }
 
