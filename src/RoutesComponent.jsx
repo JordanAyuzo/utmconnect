1b3c0d2ad3859b22b31d48f8a2ab2986f568b1ee
Alejandro Mendoza Hernandez <meha020503@gs.utm.mx>
2024-06-10 06:17:04 -0600

    Se mejoró lo de eliminar administrador y el historial de vacantes para alumnos

--- src/RoutesComponent.jsx
+++ src/RoutesComponent.jsx
@@ -12,6 +12,7 @@ import RegisterEmpresa from './components/pages/registerEmpresas/registerEmpresa
 import PageVacantes from './components/pages/pageVacantes/pageVacantes.jsx';
 import HomeEmpresa from './components/pages/homEmpresa/homeEmpresa.jsx';
 import SettingsEmpresa from './components/pages/settingsEmpresa/settingsEmpresa.jsx';
+import HistoryAlumno from './components/pages/historyAlumno/historyAlumno.jsx';
 
 function RoutesComponent() {
     return (
@@ -29,6 +30,7 @@ function RoutesComponent() {
                     <Route path="/registerEmpresa" element={<RegisterEmpresa/>}/>
                     <Route path="/pageVacantes" element={<PageVacantes/>}/>
                     <Route path="/homeEmpresa" element={<HomeEmpresa/>}/>
+                    <Route path="/historyAlumno" element={<HistoryAlumno/>}/>
                 </Routes>
         </div>
     );
---
+++ src/components/pages/historyAlumno/historyAlumno.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import NavbarStud from "@/components/layouts/navbar/navbarStud"; 
+
+
+function HistoryAlumno() {
+  return (
+    <NavbarStud />
+  )
+}
+
+export default HistoryAlumno
